82f15968f93fc60f584d12dbe1231b53f3f81db8
art-temel <59021905+art-temel@users.noreply.github.com>
2020-09-08 16:31:25 +0300

    merged server

--- flaskserver.py
+++ flaskserver.py
@@ -3,6 +3,8 @@ from flask_restful import Resource, Api
 import pymongo
 from pymongo import MongoClient
 import random
+from models import reservations
+
 
 cluster = MongoClient("mongodb+srv://ege:ege123@cluster0.smbge.gcp.mongodb.net/testdb?retryWrites=true&w=majority")
 
@@ -11,6 +13,8 @@ collection = db["reservations"]
 
 app = Flask(__name__)
 api = Api(app)
+# reservation=reservations.Reservation(reservation_type="ege",reservation_dates="12.12.12").save()
+
 
 class HelloWorld(Resource):
     def get(self):
@@ -63,3 +67,85 @@ api.add_resource(deleteres, '/deleteres/<string:month>/<string:day>/<string:hour
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+#### Model ####
+from mongoengine import *
+from datetime import datetime
+from datetime import date
+import json
+import os
+
+connect("test",host='mongodb+srv://ege:ege123@cluster0.smbge.gcp.mongodb.net')
+class User(Document):
+    username=StringField(required=True)
+    email=EmailField()
+    status=ListField()
+    date_created= DateTimeField(default=datetime.utcnow)
+
+    def json(self):
+        user_dict ={
+            "username":self.username,
+            "email":self.email
+        }
+        return json.dumps(user_dict)
+    meta={
+        "indexes":["username","email"],
+        "ordering":["-date_craeted"]
+        }
+
+class Offices(Document):
+    officeCategoryName=StringField(required=True)
+    officeType=StringField(required=True)
+    officeUsage=StringField(required=True)
+    officeCategory=ListField() #liste şeklinde olacak
+    location=StringField(required=True)
+    reservations=ReferenceField("Reservation")
+    reservation_dates=ListField()
+
+    date_created= DateTimeField(default=datetime.utcnow)
+
+    def json(self):
+        office_dict ={
+            "officeCategory":self.officeCategory,
+            "location":self.location
+        }
+        return json.dumps(office_dict)
+    meta={
+        "indexes":["officeCategoryName","officeType","officeUsage","location"],
+        "ordering":["-date_craeted"]
+        }
+
+class Reservation(Document):
+    reservation_dates=DateTimeField()
+    reservation_user=ReferenceField("User")
+    office=ReferenceField("Offices")
+
+    date_created= DateTimeField(default=datetime.utcnow)
+
+    def json(self):
+        reservation_dict ={
+            "reservation_dates":self.reservation_dates
+        }
+        return json.dumps(reservation_dict)
+    meta={
+        "indexes":["reservation_dates"],
+        "ordering":["-date_craeted"]
+        }
+
+# Creating Offices
+office1=Offices(officeCategoryName="Deniz Manzaralı Köşe Daire",officeUsage="monthly",officeType="Virtual",location="Istanbul",reservation_dates=[ "" ] ).save()
+
+# #Creating Reservation
+# def reservation_add(officeCategory):
+#     offices=Offices.objects(officeCategoryName=officeCategory)
+#     reservations=Reservation(reservation_dates=datetime(2020,10,12), office=offices[0]).save()
+#     for res in reservations:
+        
+#         offices[0].reservation_dates.apppend(datetime(2020,10,12))
+
+
+# reservation_add("Deniz Manzaralı Köşe Daire")
+
